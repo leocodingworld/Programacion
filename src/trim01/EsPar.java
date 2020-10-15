a91bd42d9821943e745900133277481b63fe1be4
cpaezd <cpaezd@hotmail.com>
2020-10-15 18:47:48 +0200

    Bloque Ejercicios 1

---
+++ src/trim01/EsPar.java
@@ -0,0 +1,22 @@
+package trim01;
+
+import java.util.Scanner;
+
+public class EsPar {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dame un número y te diré si es par");
+		int num = sc.nextInt();
+		
+		if (num % 2 == 0) {
+			System.out.println("Es par");
+		} else {
+			System.out.println("No es par");
+		}
+		
+
+	}
+
+}
