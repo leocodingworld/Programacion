d384bad1f6ad15d42a705f2419784a031a3286ab
cpaezd <cpaezd@hotmail.com>
2020-10-15 18:56:46 +0200

    Es Bisiesto añadido

---
+++ src/trim01/EsBisiesto.java
@@ -0,0 +1,21 @@
+package trim01;
+
+import java.util.Scanner;
+
+public class EsBisiesto {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Dime un año y te diré si es bisiesto: ");
+		int ahno = sc.nextInt();
+		
+		if(ahno % 4 == 0 && (ahno % 100 != 0 || ahno % 400 == 0)) {
+			System.out.println("Es Bisiesto");
+		} else {
+			System.out.println("No es Bisiesto");
+		}
+
+	}
+
+}
