538a76cef87ceef3ce254e82dd3a7dad47e8d2d6
cpaezd <cpaezd@hotmail.com>
2020-10-20 18:51:20 +0200

    Otros cambios

--- src/trim01/Eje05_InvertirCifras.java
+++ src/trim01/Eje05_InvertirCifras.java
@@ -7,22 +7,21 @@ public class Eje05_InvertirCifras {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Dame un numero ");
-		int n = sc.nextInt();
-		int n2 = n; // n2 es el número que inicialmente se introduce por teclado
-		// se guarda para compararlo más tarde
+        System.out.print("Dame un numero ");
+        int n = sc.nextInt();
+        int n2 = n; // n2 es el número que inicialmente se introduce por teclado
+        // se guarda para compararlo más tarde
+        int r = 0;
+        
+        int inv = 0;
+        
+        do {
+            r = n % 10;
+            inv = inv * 10 + r;
+            n /= 10;
+        } while (n > 0);
 
-		int r = n%10;
-		int inv = r;
-		n /= 10;
-		
-		while (n > 0){
-			r = n % 10;
-			inv = inv * 10 + r;
-			n /= 10;
-		}
-		
-		System.out.println(inv);
+        System.out.println(inv);
 
 	}
 
