1a5bd0067f126a5d10cf87ddbaa84e5b2dc2b12b
lecodingworld <okidoki0305@hotmail.com>
2021-02-05 18:00:14 +0100

    Subido ejercicio Banco

--- src/trim02/banco/Banco.java
+++ src/trim02/banco/Banco.java
@@ -1,10 +1,104 @@
 package trim02.banco;
 
+import java.util.Scanner;
+
 public class Banco {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Cliente c1 = new Cliente();
+		
+		int opt = 0;
+		String campo;
+		String pin;
+		String clave;
+		
+		do{
+			System.out.println("1.- Introducir datos del cliente");
+			System.out.println("2.- Mostrar datos del cliente");
+			System.out.println("3.- Mostrar datos del cliente (Privado)");
+			System.out.println("4.- Comprobar clave");
+			System.out.println("0.- SALIR");
+			System.out.print("Escoge una opción: ");
+			opt = Integer.parseInt(sc.next());
+			
+			switch(opt){
+				case 1:
+					
+					do{
+						System.out.print("Introduce el nombre: ");
+						campo = sc.next();
 
+						if(!Cliente.validarNombre(campo)){
+							System.out.println("El dato introducido no es válido. Vuelve a  introducirlo");
+						}
+
+					} while(!Cliente.validarNombre(campo));
+					
+					c1.setNombre(campo);
+
+					do{
+						System.out.print("Introduce el alias: ");
+						campo = sc.next();
+
+						if(!Cliente.validarAlias(campo)){
+							System.out.println("El dato introducido no es válido. Vuelve a  introducirlo");
+						}
+
+					} while(!Cliente.validarAlias(campo));
+
+					c1.setAlias(campo);					
+					
+					do{
+						System.out.print("Introduce la fecha de nacimiento: ");
+						campo = sc.next();
+
+						if(!Cliente.validarFecha(campo)){
+							System.out.println("El dato introducido no es válido. Vuelve a  introducirlo");
+						}
+
+					} while(!Cliente.validarFecha(campo));
+					
+					c1.setAnhoNac(campo);
+					c1.setClave();
+					
+					System.out.println("¡Datos guardadados!");
+					
+					break;
+				case 2:
+					System.out.println(c1.toString());
+					
+					break;
+				case 3:
+					System.out.print("Introduce el PIN: ");
+					pin = sc.next();
+					
+					System.out.println(c1.toString(pin));
+					break;
+				case 4:
+					System.out.print("Introducir clave: ");
+					clave = sc.next();
+					
+					if(clave.equals(c1.getClave())){
+						System.out.println("Bienvenido.");
+					} else {
+						System.out.println("Código Erróneo.");
+					}
+					
+					break;
+				case 0:
+					System.out.println("Hasta luego");
+					break;
+				default:
+					System.out.println("Opción no válida");
+					break;
+			}
+			
+			System.out.println("");
+
+		} while(opt != 0);
+		
 	}
-
+	
 }
+
--- src/trim02/banco/Cliente.java
+++ src/trim02/banco/Cliente.java
@@ -1,5 +1,123 @@
 package trim02.banco;
 
 public class Cliente {
+	
+	private String nombre;
+	private String alias;
+	private String anhoNac;
+	private String clave;
+	
+	private final String CLAVE_SECRETA = "Aa12";
 
+	public Cliente() {}
+
+	public Cliente(String nombre, String alias, String anhoNac) {
+		this.nombre = nombre;
+		this.alias = alias;
+		this.anhoNac = anhoNac;
+		this.clave = generarClave(alias,anhoNac);
+	}
+	
+	public Cliente(Cliente other){
+		this.nombre = other.nombre;
+		this.alias = other.alias;
+		this.anhoNac = other.anhoNac;
+		this.clave = other.clave;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public String getAnhoNac() {
+		return anhoNac;
+	}
+
+	public void setAnhoNac(String anhoNac) {
+		this.anhoNac = anhoNac;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave() {
+		this.clave = generarClave(this.alias,this.anhoNac);
+	}
+	
+	public static boolean validarNombre(String nombre){
+		boolean valido = false;
+		
+		if(nombre.matches("[aA-zZ]{5,25}")){
+			valido = true;
+		}
+		
+		return valido;
+	}
+	
+	public static boolean validarAlias(String alias){
+		boolean valido = false;
+		
+		if(alias.matches("[A-Z]{5}")){
+			valido = true;
+		}
+		
+		return valido;
+	}
+	
+	public static boolean validarFecha(String fecha){
+		boolean valido = false;
+		int cambio = Integer.parseInt(fecha);
+		
+		if(cambio < 2000){
+			valido = true;
+		}
+				
+		return valido;
+	}
+	
+	private static String generarClave(String alias, String fecha){
+		String clave = "";
+		String a = "";
+		String f = "";
+		
+		for(int i = alias.length() - 1; i >= 0; i--){
+			a += alias.charAt(i);
+		}
+		
+		for(int i = fecha.length() - 1; i >= 0; i--){
+			f += fecha.charAt(i);
+		}
+		
+		clave = f + "-" + a;
+		
+		return clave;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente{" + "nombre=" + nombre + ", alias=" + alias + ", anhoNac=" + anhoNac + '}';
+	}
+	
+	public String toString(String clave) {
+		if(clave.equals(CLAVE_SECRETA)){
+			return "Cliente{" + "nombre=" + nombre + ", alias=" + alias + ", anhoNac=" + anhoNac + ", clave=" + this.clave + '}';
+		} else {
+			System.out.println("Acceso denegado");
+			return this.toString();
+		}
+		
+	}
 }
